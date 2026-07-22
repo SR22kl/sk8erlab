4333c777aaa42ca95d3c980a9b9c21360721534a
Sumit Rathod <sbrathod22kl@gmail.com>
2026-07-22 14:34:36 +0530

    LoaderComponent added

--- slices/TeamGrid/index.tsx
+++ slices/TeamGrid/index.tsx
@@ -20,28 +20,30 @@ const TeamGrid: FC<TeamGridProps> = async ({ slice }) => {
   const skaters = await client.getAllByType("skater");
 
   return (
-    <Bounded
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-      className="bg-texture bg-brand-purple"
-    >
-      <SlideIn>
-        <Heading as="h2" size="lg" className="mb-8 text-center text-white">
-          <PrismicText field={slice.primary.heading} />
-        </Heading>
-      </SlideIn>
-      <div className="grid grid-cols-1 gap-8 md:grid-cols-4">
-        {skaters.map((skater, index) => (
-          <React.Fragment key={index}>
-            {skater.data.first_name && (
-              <SlideIn>
-                <Skaters skater={skater} index={index} />
-              </SlideIn>
-            )}
-          </React.Fragment>
-        ))}
-      </div>
-    </Bounded>
+    <div id="team">
+      <Bounded
+        data-slice-type={slice.slice_type}
+        data-slice-variation={slice.variation}
+        className="bg-texture bg-brand-purple"
+      >
+        <SlideIn>
+          <Heading as="h2" size="lg" className="mb-8 text-center text-white">
+            <PrismicText field={slice.primary.heading} />
+          </Heading>
+        </SlideIn>
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-4">
+          {skaters.map((skater, index) => (
+            <React.Fragment key={index}>
+              {skater.data.first_name && (
+                <SlideIn>
+                  <Skaters skater={skater} index={index} />
+                </SlideIn>
+              )}
+            </React.Fragment>
+          ))}
+        </div>
+      </Bounded>
+    </div>
   );
 };
 
